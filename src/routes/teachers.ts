4f0b1f9df314e385579ea5313b1285743ddfae01
cauaosp <cauaosp20@gmail.com>
2026-03-18 01:01:37 -0300

    refacotr: alterado a requisição do GET do professor para receber todos alunos e as matrículas de cada um, ajustado o atributo id para alterar a nota.

--- src/routes/teachers.ts
+++ src/routes/teachers.ts
@@ -17,14 +17,29 @@ export default async function (fastify: FastifyInstance) {
       if (user.role !== "professor" && user.role !== "admin")
         return reply.code(403).send({ error: "Acesso negado." });
 
-      const snapshot = await db
+      const usersQuery = await db
         .collection("users")
         .where("role", "==", "aluno")
         .get();
-      return snapshot.docs.map((doc) => ({
-        id: doc.id,
-        name: doc.data().name,
-      }));
+
+      const enrollmentList: any[] = [];
+      const enrollmentQuery = await db.collection("enrollments").get();
+      enrollmentQuery.forEach((doc) => {
+        enrollmentList.push({ matriculaId: doc.id, ...doc.data() });
+      });
+
+      const response: any[] = [];
+      usersQuery.forEach((user) => {
+        response.push({
+          id: user.id,
+          name: user.data().name,
+          enrollments: enrollmentList.filter(
+            (enrollment) => enrollment.studentId == user.id,
+          ),
+        });
+      });
+
+      return response;
     },
   );
 
@@ -37,9 +52,9 @@ export default async function (fastify: FastifyInstance) {
         description: "Adicionar nota final para um aluno em uma disciplina",
         body: {
           type: "object",
-          required: ["enrollment_id", "grade"],
+          required: ["enrollmentId", "grade"],
           properties: {
-            enrollment_id: { type: "string" },
+            enrollmentId: { type: "string" },
             grade: { type: "number" },
           },
         },
@@ -51,22 +66,20 @@ export default async function (fastify: FastifyInstance) {
       if (user.role !== "professor" && user.role !== "admin")
         return reply.code(403).send({ error: "Acesso negado." });
 
-      const { enrollment_id, grade } = request.body as any;
+      const { enrollmentId, grade } = request.body as any;
 
       await db
         .collection("enrollments")
-        .doc(enrollment_id)
+        .doc(enrollmentId)
         .update({
           grade: grade,
           updated_at: new Date().toISOString(),
         })
         .catch((error) => {
           console.error("Erro ao atualizar a nota:", error);
-          return reply
-            .code(500)
-            .send({
-              error: `Erro ao atualizar a nota. Verifique se essa é a matrícula correta: ${enrollment_id}`,
-            });
+          return reply.code(500).send({
+            error: `Erro ao atualizar a nota. Verifique se essa é a matrícula correta: ${enrollmentId}`,
+          });
         });
 
       return reply.code(201).send({ message: "Nota atribuída com sucesso!" });
